e2450c4987e9794f67a3f4fdd6f9c1693bee9624
AlekseyPetrik <95petrik_aleksey@mail.ru>
2019-05-13 20:07:36 +0300

    Merge branch 'master' of D:\IdeaProjects\webProject2 with conflicts.

--- src/main/java/spring/DAO/Repository/BankRepository.java
+++ src/main/java/spring/DAO/Repository/BankRepository.java
@@ -20,15 +20,9 @@ public interface BankRepository extends JpaRepository<Bank, Long> {
     @Query("select b from Bank b where b.name like %:name%")
     List<Bank> findAllByNameLike(@Param("name") String name);
 
-    Bank getBankById(Long id);
-
-    //аннотация @Async указывает, что метод должен выполняться асинхронно.
-    //@Modifying говорит о том, что указанный метод должен быть интерпретирован как модифицирующий запрос
-    //@Transactional добавляет поддержку транзакций для указанного метода
-
-    @Transactional
-    @Modifying
+//    @Query("DELETE FROM Bank b WHERE b.id in :list")
+//    void deleteByIdBank(@Param("list") List<Long> list);
     @Query("delete from Bank b where b.id in (:list)")
-    void deleteAllByid(@Param("list") List<Long> list);
+    void deleteAbc(@Param("list") List<Long> list);
 
 }
